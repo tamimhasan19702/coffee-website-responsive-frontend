4f4fc68c46c409f215248423211878938eadd8e0
tamimhasan19702 <tamimhasancu@gmail.com>
2022-06-24 22:44:36 +0600

    header section update

--- index.js
+++ index.js
@@ -24,4 +24,16 @@ const navMenu = document.querySelector('#nav-menu'),
         const navMenu = document.querySelector('#nav-menu')
         navMenu.classList.remove('show-menu')
       }
-      navLink.forEach(n => n.addEventListener('click',linkAction))
+      navLink.forEach(n => n.addEventListener('click',linkAction));
+
+
+     function scrollHeader(){
+      const header = document.querySelector('#header');
+      if(this.scrollY >= 100) {
+        header.classList.add('scroll-header');
+      } else {
+        header.classList.remove('scroll-header')
+      } 
+    }
+  
+    window.addEventListener('scroll',scrollHeader);
